2bedacd5c05126d0a042ecab30b38f7e52c61694
Tempted <2374704101@qq.com>
2022-11-28 21:07:05 +0800

    second idea commit

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git!!!");
+        System.out.println("I love hjj for the reason that she is very beautiful");
     }
 }
