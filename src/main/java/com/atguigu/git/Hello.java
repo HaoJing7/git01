d23997da0e384033101f3a970bfc0a76e1d9a732
Tempted <2374704101@qq.com>
2022-11-28 21:40:56 +0800

    hot-fix2 idea commit

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("I love hjj for the reason that she is very beautiful");
         System.out.println("Hello git3!!!");
         System.out.println("git 4!!!");
+        System.out.println("hot-fix 4!!!");
     }
 }
